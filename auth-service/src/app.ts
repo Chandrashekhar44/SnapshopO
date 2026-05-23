ce1536d82887fbddbfd99868061313ab41599329
Chandra Shekhar <banothuchandu77@gmail.com>
2026-05-23 17:14:13 +0530

    feat: configure dotenv for environment variable management in the application

--- auth-service/src/app.ts
+++ auth-service/src/app.ts
@@ -1,10 +1,22 @@
+import express, { Express } from "express";
+import authRoutes from "./routes/auth.routes";
+import cors from "cors";
 
-import express,{ Express}from "express"
+const app: Express = express();
 
-const app:Express = express();
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
-app.get('/',(req,res)=>{
-    res.send("server is running")
+app.use(express.json());
 
-})
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
+
 export default app;
