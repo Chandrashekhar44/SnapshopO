4fbe504e7dfa23cfc62fc4ded082385c28c5e2fc
Chandra Shekhar <banothuchandu77@gmail.com>
2026-05-25 10:26:41 +0530

    feat: implement role access middleware for user authorization

---
+++ shopping-service/src/middleware/roleAccess.middleware.ts
@@ -0,0 +1,40 @@
+import { prisma } from "../index.js";
+import ApiError from "../utils/ApiError.js";
+import asynchandler from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
+
+interface JwtPayloadType {
+  id: number;
+}
+const roleAccessMiddleware = asynchandler(async(req,res,next)=>{
+
+    const token = req.cookies?.accessToken || req.header('Authorization')?.replace("Bearer ","")
+
+    if(!token){
+        throw new ApiError(404,"Unauthorized request");
+    }
+
+    const decodedToken = jwt.verify(token,process.env.ACCESS_TOKEN_SECRET!) as JwtPayloadType
+    if(!decodedToken?.id){
+        throw new ApiError(401,"unauthorized request")
+    }
+
+    const role = decodedToken.role
+
+    const user = await prisma.user.findUnique({
+        where:{
+            id : decodedToken.id
+        },
+    }
+    )
+
+    if(!user){
+        throw new ApiError(400,"user not found")
+
+    }
+
+    req.user = user;
+
+    next();
+
+})
