96df7ac449ee25de905dea002036ede83151cd3c
Chandra Shekhar <banothuchandu77@gmail.com>
2026-05-31 23:05:07 +0530

    feat: update Prisma schema for Conversations and Messages models, add user relationships

--- messaging-service/src/config/socket.ts
+++ messaging-service/src/config/socket.ts
@@ -1,48 +1,26 @@
 import { Server, Socket } from "socket.io";
-import http from "http";
-
 import { socketAuth } from "../socket/middleware/socketAuth";
-import { negotiationHandler } from "../services/message.services";
-import { directChatHandler } from "../services/message.services";
+import { negotiationHandler, directChatHandler } from "../services/message.services";
 
-export const setupSocket = (
-  server: http.Server
-) => {
 
+
+export const setupSocket = (server: any) => {
   const io = new Server(server, {
-    cors: {
-      origin: "*",
-      credentials: true,
-    },
+    cors: { origin: "http://localhost:3000", credentials: true },
   });
 
-  
   io.use(socketAuth);
 
   io.on("connection", (socket: Socket) => {
+    console.log(`User Connected: ${socket.user.id}`);
 
-    console.log(
-      `User Connected: ${socket.user.id}`
-    );
+    socket.join(`user:${socket.user.id}`);
 
-    
-    socket.join(
-      `user:${socket.user.id}`
-    );
-
-    
     negotiationHandler(io, socket);
-
-    
     directChatHandler(io, socket);
 
-   
     socket.on("disconnect", () => {
-
-      console.log(
-        `Disconnected: ${socket.user.id}`
-      );
-
+      console.log(`User Disconnected: ${socket.user?.id || "unknown"}`);
     });
   });
 
