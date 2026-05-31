671bdb9d4204b8c7c9971346f274b56578d407b8
Chandra Shekhar <banothuchandu77@gmail.com>
2026-05-31 23:06:30 +0530

    feat: add conversation routes for managing seller threads and searching users

---
+++ messaging-service/src/routes/conversation.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { findOrCreateConversation, getConversationMessages, getSellerThreads } from "../controller/coversation.controller";
+import { searchUsers } from "../controller/search.controller";
+
+const router = Router();
+
+router.get("/seller/:sellerId", getSellerThreads);
+router.get("/conversations/find", findOrCreateConversation);
+router.get("/conversations/:id/messages", getConversationMessages);
+router.get("/search", searchUsers);
+
+export default router;
