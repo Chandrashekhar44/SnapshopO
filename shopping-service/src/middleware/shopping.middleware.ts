1b9c41800a7972f0f5eb5eb7ab619bd03a998343
Chandra Shekhar <banothuchandu77@gmail.com>
2026-07-05 21:12:49 +0530

    feat: enhance auth middleware with improved error handling and logging

--- shopping-service/src/middleware/shopping.middleware.ts
+++ shopping-service/src/middleware/shopping.middleware.ts
@@ -1,38 +1,57 @@
-import { prisma } from "../index.js";
-import ApiError from "../utils/ApiError.js";
-import asynchandler from "../utils/asyncHandler.js";
-import jwt from "jsonwebtoken";
+import { prisma } from "../index";
+import jwt, { TokenExpiredError, JsonWebTokenError } from "jsonwebtoken";
+import ApiError from "../utils/ApiError";
+import asynchandler from "../utils/asyncHandler";
 
 interface JwtPayloadType {
   id: number;
 }
-const authMiddleware = asynchandler(async(req,res,next)=>{
 
-    const token = req.cookies?.accessToken || req.header('Authorization')?.replace("Bearer ","")
+export const authMiddleware = asynchandler(async (req, res, next) => {
+  const token =
+    req.cookies?.accessToken ||
+    req.header("Authorization")?.replace("Bearer ", "");
+    console.log("req.cookies",req.cookies)
+    console.log("req.cookies.accesstoken",req.cookies.accessToken)
 
-    if(!token){
-        throw new ApiError(404,"Unauthorized request");
+  if (!token) {
+    throw new ApiError(401, "Access token missing");
+  }
+
+  console.log("token",token)
+
+  let decodedToken: JwtPayloadType;
+
+  try {
+    decodedToken = jwt.verify(
+      token,
+      process.env.ACCESS_TOKEN_SECRET!
+    ) as JwtPayloadType;
+    console.log("decoded" ,decodedToken)
+
+  } catch (err) {
+
+    if (err instanceof TokenExpiredError) {
+      throw new ApiError(401, "Access token expired");
     }
 
-    const decodedToken = jwt.verify(token,process.env.ACCESS_TOKEN_SECRET!) as JwtPayloadType
-    if(!decodedToken?.id){
-        throw new ApiError(401,"unauthorized request")
+    if (err instanceof JsonWebTokenError) {
+      throw new ApiError(401, "Invalid access token");
     }
 
-    const user = await prisma.user.findUnique({
-        where:{
-            id : decodedToken.id
-        },
+    throw new ApiError(401, "Unauthorized token error");
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: decodedToken.id,
     }
-    )
+  });
 
-    if(!user){
-        throw new ApiError(400,"user not found")
+  if (!user) {
+    throw new ApiError(401, "User not found");
+  }
 
-    }
-
-    req.user = user;
-
-    next();
-
-})
+  req.user = user;
+  next();
+});
